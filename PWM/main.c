8b641d8cfce40a957d2d267875b002724d40aae9
dangovsky <tenkovdaniil@gmail.com>
2018-11-28 19:47:25 +0300

    added lab 4 classwork

--- PWM/main.c
+++ PWM/main.c
@@ -1,74 +1,107 @@
 #include "main.h"
 
-#include <string.h>
-
 #define LED_COLORS_LENGTH 10
 #define TIM1_PERIOD 16400
 #define TIM1_PRESCALER 1
 #define TIM1_PULSE 16236
 #define STEP 164
+#define TIM2_PERIOD 50
+#define TIM2_PRESCALER 42000
 
 /* color to incriment */
-uint8_t current_color = 0;
+static uint8_t current_color;
 /* leds "brightness" */
-uint32_t leds_compare [3] = {100, 100, 100};
+static uint32_t leds_compare [3];
 /* system led colors RGB */
-uint32_t sys_leds[3] = { GPIO_Pin_14, GPIO_Pin_12, GPIO_Pin_15 };
+static uint32_t sys_leds[3];
+/* timer for buttons*/
+static uint32_t timer_count;
+static uint32_t timer_firstb;
+static uint32_t timer_secb;
+
+void TIM2_IRQHandler(void)
+{
+   if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+   {
+      timer_count++;
+      TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+   }
+}
+
 
 void EXTI0_IRQHandler(void)
-{   
+{  
    if (EXTI_GetITStatus(EXTI_Line0) != RESET)
    {
-      if (++current_color > 2)
+      if (timer_count - timer_firstb > 5)
       {
-         current_color = 0;
+         if (++current_color > 2)
+         {
+            current_color = 0;
+         }
+         /* Turn all the leds off */
+         GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+         GPIO_SetBits(GPIOD, sys_leds[current_color]);
       }
-      /* Turn all the leds off */
-      GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-      GPIO_SetBits(GPIOD, sys_leds[current_color]);
-      
-      EXTI_ClearITPendingBit(EXTI_Line0);      
+      EXTI_ClearITPendingBit(EXTI_Line0);         
+      timer_count = timer_firstb;
    }
 }
 
 void EXTI1_IRQHandler(void)
 {   
    if (EXTI_GetITStatus(EXTI_Line1) != RESET)
-   {  
-      leds_compare[current_color] -= 10;   
-      if (leds_compare[current_color] == 0) 
-      {
-         leds_compare[current_color] = 90;
-      }
-      switch (current_color)
-      {
-         case 0:
-            TIM_SetCompare1(TIM1, leds_compare[current_color] * STEP);
-            break;
-         case 1:
-            TIM_SetCompare2(TIM1, leds_compare[current_color] * STEP);
-            break;
-         case 2:
-            TIM_SetCompare3(TIM1, leds_compare[current_color] * STEP);
-            break;
-      }
-      
+   {
+      if (timer_count - timer_secb > 5)
+      {  
+         leds_compare[current_color] -= 10;   
+         if (leds_compare[current_color] == 0) 
+         {
+            leds_compare[current_color] = 90;
+         }
+         switch (current_color)
+         {
+            case 0:
+               TIM_SetCompare1(TIM1, leds_compare[current_color] * STEP);
+               break;
+            case 1:
+               TIM_SetCompare2(TIM1, leds_compare[current_color] * STEP);
+               break;
+            case 2:
+               TIM_SetCompare3(TIM1, leds_compare[current_color] * STEP);
+               break;
+         }
+      }      
       EXTI_ClearITPendingBit(EXTI_Line1);      
+      timer_count = timer_secb;
    }
 }
 
 int main(void)
 {
-  GPIO_InitTypeDef GPIO_InitStructure;
-  TIM_TimeBaseInitTypeDef tim_struct;
-  TIM_OCInitTypeDef TIM_OCInitStruct;
-  EXTI_InitTypeDef EXTI_InitStruct;  
-  NVIC_InitTypeDef nvic_struct;
+  current_color = 0;    
+  leds_compare[0] = 100;
+  leds_compare[1] = 100;
+  leds_compare[2] = 100;
+  sys_leds[0] = GPIO_Pin_14;
+  sys_leds[1] = GPIO_Pin_12; 
+  sys_leds[2] = GPIO_Pin_15;
+  timer_count = 0;
+  timer_firstb = 0;
+  timer_secb = 0;
+
+  GPIO_InitTypeDef GPIO_InitStructure = {0};
+  TIM_TimeBaseInitTypeDef tim_struct = {0};
+  TIM_OCInitTypeDef TIM_OCInitStruct = {0};
+  EXTI_InitTypeDef EXTI_InitStruct = {0};  
+  NVIC_InitTypeDef nvic_struct = {0};
 
   /* Enable peripheral clock for LEDs port */
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-  /* Enable peripheral clock for timer 2*/
+  /* Enable peripheral clock for timer 1*/
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+  /* Enable peripheral clock for timer 2*/
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
   /* Enable peripheral clock for buttons */
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
   /* Enable peripheral clock for SYSCFG */
@@ -98,7 +131,7 @@ int main(void)
   EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
   EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
   EXTI_Init(&EXTI_InitStruct);
-  /*  Configurate interrupts for button 0 */
+  /* Configurate interrupts for button 0 */
   nvic_struct.NVIC_IRQChannel = EXTI0_IRQn;
   nvic_struct.NVIC_IRQChannelPreemptionPriority = 0;
   nvic_struct.NVIC_IRQChannelSubPriority = 0;
@@ -128,10 +161,24 @@ int main(void)
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOA, &GPIO_InitStructure);  
+  /* Init timer 2 */
+  tim_struct.TIM_Period        = TIM2_PERIOD - 1;
+  tim_struct.TIM_Prescaler     = TIM2_PRESCALER - 1;
+  tim_struct.TIM_ClockDivision = 0;
+  tim_struct.TIM_CounterMode   = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM2, &tim_struct);
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+  TIM_Cmd(TIM2, ENABLE);
+  /* Configurate interrupts for timer 2 */
+  nvic_struct.NVIC_IRQChannel = TIM2_IRQn;
+  nvic_struct.NVIC_IRQChannelPreemptionPriority = 0;
+  nvic_struct.NVIC_IRQChannelSubPriority = 1;
+  nvic_struct.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&nvic_struct);
   /* Init timer 1 */
   tim_struct.TIM_Period        = TIM1_PERIOD - 1;
   tim_struct.TIM_Prescaler     = TIM1_PRESCALER - 1;
-  tim_struct.TIM_ClockDivision = TIM_CKD_DIV1;
+  tim_struct.TIM_ClockDivision = 0;
   tim_struct.TIM_CounterMode   = TIM_CounterMode_Up;
   TIM_TimeBaseInit(TIM1, &tim_struct);
   TIM_Cmd(TIM1, ENABLE);
@@ -147,6 +194,7 @@ int main(void)
   TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
   TIM_OC3Init(TIM1, &TIM_OCInitStruct);
   TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
   TIM_CtrlPWMOutputs(TIM1, ENABLE);
 
   /* Turn all the leds off */
@@ -155,16 +203,13 @@ int main(void)
 
   while (1)
   {
-
 /*
-     GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-     GPIO_SetBits(GPIOD, sys_leds[current_color]);
-*/
-     for(uint32_t i = 0; i < 16400; i+=STEP){
+     for(uint32_t i = 0; i < 16400; i+=step){
          TIM_SetCompare1(TIM1, i);
          TIM_SetCompare2(TIM1, i);
          TIM_SetCompare3(TIM1, i);
          for (int k = 0; k < 160000; k++);
-     }  
+     }
+*/
   }
 }
