16bc86e9d6e5c2d58f3cfb11b93ac7dd782c8aad
dangovsky <tenkovdanill@gmail.com>
2018-12-13 22:40:43 +0300

    add comments and documentation

--- libled/include/led.h
+++ libled/include/led.h
@@ -1,20 +1,36 @@
-#ifndef LEDS_AND_BUTTON_H
-#define LEDS_AND_BUTTON_H
+#ifndef LIBLED_H 
+#define LIBLED_H 
 
 #include <stm32f4xx.h>
 
-/* #if defined (USE_STM324xG_EVAL) */
-/*   #include "stm324xg_eval.h" */
+/*! \file led.h
+ *  \brief API for libled
+ *  
+ *  DSR ESTC course 5-th task
+ */
 
-/* #elif defined (USE_STM32F4DISC_EVAL) */
-/*   #include "stm32f4disc_eval.h" */
-
-/* #else */
-/*  #error "Please select first the Evaluation board used in your application (in Project Options)" */
-/* #endif */
-
+/*! \brief Initialise function.
+ *
+ *  Initialise timer, PWM and Pins.
+ *  * Timer - TIM1.
+ *  * PWM - from TIM1 with pins AF.
+ *  * GROIA Pins - 8, 9, 10.
+ */
 void InitLeds(void);
+
+/*! \brief Set choosen LEDs color
+ *
+ *  Calculate and set comparators to TIM1's PWM.
+ *  Fixes green color brightness.
+ *  \param red, green, blue - brightness of components
+ */
 void SetColorRGB(uint8_t red, uint8_t green, uint8_t blue);
+
+/*! \brief Set choosen LEDs color
+ *
+ *  Split color on components and calls SetColorRGB.
+ *  \param color - 24-bit HEX code.
+ */
 void SetColorHex(uint32_t color);
 
-#endif  /* LEDS_AND_BUTTON_H */
+#endif  /* LIBLED_H */
