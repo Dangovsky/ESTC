44f16aa8a6f3aff36806690f842c6015ed64c7df
dangovsky <tenkovdanill@gmail.com>
2019-01-16 20:24:45 +0300

    remove html documentation

--- libled/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['initleds',['InitLeds',['../led_8h.html#a303ace2ebdaec7d4292fc7dccbbaf4ca',1,'led.h']]]
-];
--- libled/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['setcolorhex',['SetColorHex',['../led_8h.html#adb69a2a4a6868b2b43cc79bdf0f02bbf',1,'led.h']]],
-  ['setcolorrgb',['SetColorRGB',['../led_8h.html#a0b0bdc049fa0bc2e46f057b74108d69a',1,'led.h']]]
-];
